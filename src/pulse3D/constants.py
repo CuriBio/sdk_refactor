c790fc895a3a848e51cad0bc1c6543a1afc96f3a
Jason Person <jrperson@gmail.com>
2021-12-03 11:19:17 -0800

    Add names for new metrics

--- src/pulse3D/constants.py
+++ src/pulse3D/constants.py
@@ -262,6 +262,8 @@ CALCULATED_METRIC_DISPLAY_NAMES = {
     WIDTH_UUID: "Twitch Width {} (seconds)",
     RELAXATION_TIME_UUID: "Time From Peak to Relaxation {} (seconds)",
     CONTRACTION_TIME_UUID: "Time From Contraction {} to Peak (seconds)",
+    BASELINE_TO_PEAK_UUID: "Baseline to Peak",
+    PEAK_TO_BASELINE_UUID: "Peak to Baseline",
 }
 
 COORDS = (10, 25, 50, 75, 90)

0c3026e0bf1e14187ded9093fe26fbca94f7204f
nikitapoly <nikitapoly@comcast.net>
2022-09-14 15:22:51 -0700

    removed extra param in yield call

--- src/pulse3D/plate_recording.py
+++ src/pulse3D/plate_recording.py
@@ -516,15 +516,16 @@ class PlateRecording:
     def from_directory(path):
         # multi zip files
         for zf in glob.glob(os.path.join(path, "*.zip"), recursive=True):
-            zip_file = zipfile.ZipFile(zf)
-            # check if this is a zip of optical files
-            if ".xlsx" in zip_file.namelist()[0]:
-                with tempfile.TemporaryDirectory() as tmpdir:
-                    zip_file.extractall(path=tmpdir)
-                    for optical_file in os.scandir(tmpdir):
-                        yield PlateRecording(optical_file.path, calc_time_force)
-            log.info(f"Loading recording from file {zf}")
-            yield PlateRecording(zf, calc_time_force)
+            for zf in glob.glob(os.path.join(path, "*.zip"), recursive=True):
+                zip_file = zipfile.ZipFile(zf)
+                # check if this is a zip of optical files
+                if ".xlsx" in zip_file.namelist()[0]:
+                    with tempfile.TemporaryDirectory() as tmpdir:
+                        zip_file.extractall(path=tmpdir)
+                        for optical_file in os.scandir(tmpdir):
+                            yield PlateRecording(optical_file.path)
+                log.info(f"Loading recording from file {zf}")
+                yield PlateRecording(zf)
 
         # multi optical files
         for of in glob.glob(os.path.join(path, "*.xlsx"), recursive=True):

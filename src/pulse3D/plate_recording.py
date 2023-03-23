ee86c56cec1ba5229faf6066cb4e72087095d632
nikitapoly <nikitapoly@comcast.net>
2023-03-23 12:59:42 -0700

    pre-commit

--- src/pulse3D/plate_recording.py
+++ src/pulse3D/plate_recording.py
@@ -377,7 +377,9 @@ class PlateRecording:
             raise NoRecordingFilesLoadedError()
 
         # set up platemap info
-        first_avaliable_well = [self.wells[well] for well in range(len(self.wells)) if self.wells[well] is not None][0]
+        first_avaliable_well = [
+            self.wells[well] for well in range(len(self.wells)) if self.wells[well] is not None
+        ][0]
         self.platemap_name = first_avaliable_well[PLATEMAP_NAME_UUID]
         platemap_labels = defaultdict(list)
 

7140cdc9cbe81c1dea0aa55e694b17f343ca1190
Luci Pak <luciypak@gmail.com>
2023-04-29 13:33:29 +1000

    bump to 0.33.0

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ if USE_CYTHON:
 
 setup(
     name="Pulse3D",
-    version="0.32.7",
+    version="0.33.0",
     description="Pulse3D Analysis Platform",
     url="https://github.com/CuriBio/Pulse3D",
     project_urls={"Documentation": "https://pulse3D.readthedocs.io/en/latest/"},
